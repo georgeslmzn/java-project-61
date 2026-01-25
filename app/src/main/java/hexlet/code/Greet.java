61b67f5c07c9bb25a1ba2c74264d258418cc9971
george <georgeslmzn@gmail.com>
2026-01-25 14:11:25 +0400

    * Add Greet

---
+++ app/src/main/java/hexlet/code/Greet.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+
+public class Greet {
+    public static String getName() {
+        Cli.printMsg("Welcome to the Brain Games!");
+        Cli.printMsg("May I have your name?");
+        return Cli.getString();
+    }
+
+    public static void run() {
+        Cli.printMsg("Welcome to the Brain Games!");
+        Cli.printMsg("May I have your name?");
+        String name = Cli.getString();
+        Cli.printMsg("Hello, " + name + "!");
+    }
+}
